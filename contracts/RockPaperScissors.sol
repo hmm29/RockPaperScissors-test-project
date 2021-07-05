80b57c7493dc3caf6273cb16fa6f316582d96dff
Harrison Miller <hmaxmiller@gmail.com>
2021-07-04 19:52:23 -0600

    Add openzeppelin package, add parent classes to RockPaperScissors contract

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RockPaperScissors {
+abstract contract RockPaperScissors is ERC20, Ownable {
     uint256 public feeInTokens;
 
     constructor(uint256 _feeInTokens) {
